e855a601784a1d74b77b8929764d10f8c3d8494a
Gabijesumary <gabriela.jesumary@gmail.com>
2022-06-10 14:04:27 -0300

    Alteração App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Torch from 'react-native-torch';
 import RNShake from 'react-native-shake';
@@ -13,36 +13,36 @@ const App = () => {
     Torch.switchState(toggle);
   }, [toggle]);
 
-  useEffect (() => {
+  useEffect(() => {
     //Quando o celular for chacoalhado
     const subscription = RNShake.addListener(() => {
       setToggle(oldToggle => !oldToggle);
     });
     //Essa func vai ser chamada qnd o componente for desmontado
     return () => subscription.remove();
-  },[]);
+  }, []);
 
   return (
     <View style={toggle ? style.containerLight : style.container}>
       <TouchableOpacity onPress={handleChangeToggle}>
-          <Image
-            style={toggle ? style.lightingOn : style.lightingOff}
-            source={
-              toggle
+        <Image
+          style={toggle ? style.lightingOn : style.lightingOff}
+          source={
+            toggle
               ? require('./assets/icons/eco-light.png')
               : require('./assets/icons/eco-light-off.png')
-            }
-            />
-            <Image
-            style={style.dioLogo}
-            source={
-              toggle
+          }
+        />
+        <Image
+          style={style.dioLogo}
+          source={
+            toggle
               ? require('./assets/icons/logo-dio.png')
               : require('./assets/icons/logo-dio-white.png')
-            }
-          />         
-       </TouchableOpacity>
-     </View>
+          }
+        />
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -80,5 +80,4 @@ const style = StyleSheet.create({
     width: 250,
     height: 250,
   },
-
 });
